d026ffe44245a1e8c8b39709dee794ff949d5a5f
Ludovic Saule <ludovic_saule@hotmail.fr>
2024-11-29 10:56:42 +0100

    controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
+  get 'bookmarks/new'
+  get 'bookmarks/create'
+  get 'lists/index'
+  get 'lists/create'
+  get 'lists/new'
+  get 'lists/show'
 resources :movies, only: [:index, :show]
 
 resources :lists, only: [:index, :show, :new, :create] do
-  resources :bookmarks, only: [:new, :create]
+  resources :bookmarks, only: [:new, :create, :destroy]
 end
+
+resources :bookmarks, only: [:destroy]
 
 end
--- db/migrate/20241128134502_add_movie_and_list_to_bookmarks.rb
+++
@@ -1,6 +0,0 @@
-class AddMovieAndListToBookmarks < ActiveRecord::Migration[7.1]
-  def change
-    add_column :bookmarks, :movie_id, :integer
-    add_column :bookmarks, :list_id, :integer
-  end
-end
